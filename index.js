9f075df138a26204f248bf2b61cdee945a22efe6
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-11 15:06:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ class Formatter {
 
   static sanitize(string) {
     return string.replace(/[^A-Za-z0-9- ']+/g, "");
-    }
+  }
+
+  static (words) {
+    'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'
+  }
 }
