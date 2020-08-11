429719c75bdbd7cffaee97408b1875a3414cd0c3
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-11 14:38:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ class Formatter {
   static capitalize(string) {
     return string[0].toUpperCase() + string.slice(1);
   }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, '')
+  }
 }
